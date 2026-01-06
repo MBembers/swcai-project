b18863420d58b9776bcd861ceafe2bb56bd2a427
Anders200 <michal.naworyta@gmail.com>
2026-01-06 16:09:06 +0100

    cleanup

--- src/agents.py
+++ src/agents.py
@@ -11,20 +11,4 @@ class Truck:
         self.route: List[int] = []  # List of Bin IDs
         self.distance_traveled = 0.0
 
-    def move_to(self, target_pos: Tuple[float, float]):
-        from .utils import euclidean_distance
-        self.distance_traveled += euclidean_distance(self.pos, target_pos)
-        self.pos = target_pos
 
-    def collect(self, waste_bin: Bin):
-        amount = min(waste_bin.fill_level, self.capacity - self.current_load)
-        waste_bin.fill_level -= amount
-        self.current_load += amount
-
-    def is_full(self) -> bool:
-        return self.current_load >= self.capacity
-
-    def reset(self):
-        self.pos = self.start_pos
-        self.current_load = 0.0
-        self.distance_traveled = 0.0
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,3 @@ def euclidean_distance(p1: tuple[float, float], p2: tuple[float, float]) -> floa
     """Calculates the Euclidean distance between two points."""
     return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
-def manhattan_distance(p1: tuple[float, float], p2: tuple[float, float]) -> float:
-    """Calculates the Manhattan distance between two points."""
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
